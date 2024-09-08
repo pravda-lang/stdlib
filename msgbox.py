acfc5de6f3a1d50bac09c3ab89042e880b73e830
梶塚太智 <kajizukataichi@outlook.jp>
2024-09-08 14:34:29 +0900

    Create msgbox.py

---
+++ msgbox.py
@@ -0,0 +1,6 @@
+tkinter
+def main(msg):
+    master = tkinter.Tk()
+    master.title("Pravda GUI System")
+    tkinter.Label(master, text=msg, font=("Helvetica", 36, "normal")).pack()
+    master.mainloop()
