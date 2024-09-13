79e7810d8685e0e360fc3716629bf78108d60274
梶塚太智 <kajizukataichi@outlook.jp>
2024-09-13 21:24:03 +0900

    Delete msgbox.py

--- msgbox.py
+++
@@ -1,7 +0,0 @@
-import tkinter
-
-def main(msg):
-    master = tkinter.Tk()
-    master.title("Pravda GUI System")
-    tkinter.Label(master, text=msg, font=("Helvetica", 36, "normal")).pack()
-    master.mainloop()
